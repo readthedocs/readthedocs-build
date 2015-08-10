000a522e62f34ca853815c2ffd21ffa602c6d936
Gregor Müllegger <gregor@muellegger.de>
2015-08-10 12:56:36 +0200

    Add find_all logic

---
+++ readthedocs_build/config/find.py
@@ -0,0 +1,7 @@
+import os
+
+
+def find_all(path, filename):
+    for root, dirs, files in os.walk(path):
+        if filename in files:
+            yield os.path.abspath(os.path.join(root, filename))

671372befc9760458e8158e70a7a7bb35ab7a612
Gregor Müllegger <gregor@muellegger.de>
2015-08-13 11:44:00 +0200

    Create virtualenv during build

--- readthedocs_build/builder/base.py
+++ readthedocs_build/builder/base.py
@@ -1,9 +1,27 @@
+import shutil
+import subprocess
+import tempfile
+
+
+def run(args):
+    popen = subprocess.Popen(args)
+    return popen.wait()
+
+
 class BaseBuilder(object):
     def __init__(self, build_config):
         self.build_config = build_config
 
     def setup(self):
-        pass
+        self.setup_virtualenv()
+
+    def setup_virtualenv(self):
+        self.virtualenv_path = tempfile.mkdtemp()
+        run([
+            'virtualenv',
+            '--interpreter=/usr/bin/python2.7',
+            self.virtualenv_path,
+        ])
 
     def build(self):
         """
@@ -18,4 +36,4 @@ class BaseBuilder(object):
         pass
 
     def cleanup(self):
-        pass
+        shutil.rmtree(self.virtualenv_path)
--- readthedocs_build/builder/test_base.py
+++ readthedocs_build/builder/test_base.py
@@ -1,4 +1,5 @@
 from mock import patch
+import os
 
 from ..testing.config import get_project_config
 from .base import BaseBuilder
@@ -9,26 +10,57 @@ def test_build_calls_setup():
         'type': 'sphinx'
     })
     with patch.object(BaseBuilder, 'setup') as setup:
-        builder = BaseBuilder(build_config=build_config)
-        builder.build()
-        setup.assert_called_with()
+         with patch.object(BaseBuilder, 'cleanup'):
+            builder = BaseBuilder(build_config=build_config)
+            builder.build()
+            setup.assert_called_with()
 
 
 def test_build_calls_cleanup():
     build_config = get_project_config({
         'type': 'sphinx'
     })
-    with patch.object(BaseBuilder, 'cleanup') as cleanup:
-        builder = BaseBuilder(build_config=build_config)
-        builder.build()
-        cleanup.assert_called_with()
+    with patch('readthedocs_build.builder.base.run'):
+        with patch.object(BaseBuilder, 'cleanup') as cleanup:
+            builder = BaseBuilder(build_config=build_config)
+            builder.build()
+            cleanup.assert_called_with()
+        # Do real cleanup.
+        builder.cleanup()
 
 
 def test_build_calls_build_html():
     build_config = get_project_config({
         'type': 'sphinx'
     })
-    with patch.object(BaseBuilder, 'build_html') as build_html:
-        builder = BaseBuilder(build_config=build_config)
-        builder.build()
-        build_html.assert_called_with()
+    with patch('readthedocs_build.builder.base.run'):
+        with patch.object(BaseBuilder, 'build_html') as build_html:
+            builder = BaseBuilder(build_config=build_config)
+            builder.build()
+            build_html.assert_called_with()
+
+
+def test_setup_creates_virtualenv():
+    build_config = get_project_config({'type': 'sphinx'})
+    builder = BaseBuilder(build_config=build_config)
+    with patch('readthedocs_build.builder.base.run') as run:
+        builder.setup()
+        run.assert_called_with([
+            'virtualenv',
+            '--interpreter=/usr/bin/python2.7',
+            builder.virtualenv_path,
+        ])
+        assert os.path.exists(builder.virtualenv_path)
+    # Do real cleanup.
+    builder.cleanup()
+
+
+def test_cleanup_removes_virtualenv(tmpdir):
+    venv = tmpdir.mkdir('venv')
+    build_config = get_project_config({'type': 'sphinx'})
+    builder = BaseBuilder(build_config=build_config)
+    with patch.object(BaseBuilder, 'setup_virtualenv'):
+        builder.setup()
+        builder.virtualenv_path = str(venv)
+        builder.cleanup()
+        assert not venv.exists()

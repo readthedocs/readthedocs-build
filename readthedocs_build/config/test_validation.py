bc1a580b9579fe65cfc9894916b1cbe07940d500
Gregor Müllegger <gregor@muellegger.de>
2015-08-14 11:38:27 +0200

    Add validate_choice

--- readthedocs_build/config/test_validation.py
+++ readthedocs_build/config/test_validation.py
@@ -1,8 +1,10 @@
 from pytest import raises
 
 from .validation import validate_bool
+from .validation import validate_choice
 from .validation import ValidationError
 from .validation import INVALID_BOOL
+from .validation import INVALID_CHOICE
 
 
 def describe_validate_bool():
@@ -22,3 +24,18 @@ def describe_validate_bool():
         with raises(ValidationError) as excinfo:
             validate_bool('random string')
         assert excinfo.value.code == INVALID_BOOL
+
+
+def describe_validate_choice():
+
+    def it_accepts_valid_choice():
+        result = validate_choice('choice', ('choice', 'another_choice'))
+        assert result is 'choice'
+
+        result = validate_choice('c', 'abc')
+        assert result is 'c'
+
+    def it_rejects_invalid_choice():
+        with raises(ValidationError) as excinfo:
+            validate_choice('not-a-choice', ('choice', 'another_choice'))
+        assert excinfo.value.code == INVALID_CHOICE
--- readthedocs_build/config/validation.py
+++ readthedocs_build/config/validation.py
@@ -1,9 +1,11 @@
 INVALID_BOOL = 'invalid-bool'
+INVALID_CHOICE = 'invalid-choice'
 
 
 class ValidationError(Exception):
     messages = {
         INVALID_BOOL: 'expected one of (0, 1, true, false), got {value}',
+        INVALID_CHOICE: 'expected one of ({choices}), got {value}',
     }
 
     def __init__(self, value, code, format_kwargs=None):
@@ -18,7 +20,15 @@ class ValidationError(Exception):
         super(ValidationError, self).__init__(message)
 
 
+def validate_choice(value, choices):
+    if value not in choices:
+        raise ValidationError(value, INVALID_CHOICE, {
+            'choices': ', '.join(choices)
+        })
+    return value
+
+
 def validate_bool(value):
-    if value in (0, 1, False, True):
-        return bool(value)
-    raise ValidationError(value, INVALID_BOOL)
+    if value not in (0, 1, False, True):
+        raise ValidationError(value, INVALID_BOOL)
+    return bool(value)

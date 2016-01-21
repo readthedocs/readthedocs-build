d803937d7db8347cdeced712ca9c73b37860347c
Eric Holscher <eric@ericholscher.com>
2016-01-21 15:25:54 -0800

    Don't default formats

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='readthedocs-build',
-    version='2.0.1',
+    version='2.0.2',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='https://readthedocs.org',

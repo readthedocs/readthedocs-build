48f6bb24b2b61cb6aa883d751833ca9ee42581b7
Anthony Johnson <aj@ohess.org>
2017-02-09 12:05:19 -0800

    Use topdown to sort find files
    Because os.walk returns files in an arbitrary manner, sort dirs to ensure this
    order. Also tunes up tox config more.

--- readthedocs_build/config/find.py
+++ readthedocs_build/config/find.py
@@ -3,7 +3,8 @@ import os
 
 def find_all(path, filenames):
     path = os.path.abspath(path)
-    for root, dirs, files in os.walk(path):
+    for root, dirs, files in os.walk(path, topdown=True):
+        dirs.sort()
         for filename in filenames:
             if filename in files:
                 yield os.path.abspath(os.path.join(root, filename))

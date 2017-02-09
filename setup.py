73d3036efab80de5b13839f6a83ac690bd1a0eae
Anthony Johnson <aj@ohess.org>
2017-02-08 19:53:02 -0800

    Release 2.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='readthedocs-build',
-    version='2.0.5',
+    version='2.0.6',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='https://readthedocs.org',

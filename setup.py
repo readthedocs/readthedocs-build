1cc26dbed7486abedd6f2fb2339775cab8477275
Eric Holscher <eric@ericholscher.com>
2018-01-04 12:24:38 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='readthedocs-build',
-    version='2.0.8',
+    version='2.0.9',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='https://readthedocs.org',
